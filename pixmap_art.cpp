8193ec67d435e4820609e2607ec7408b58725420
Yuxiao Wang <yx99.wang@gmail.com>
2021-03-05 23:34:09 -0500

    update name

--- pixmap_art.cpp
+++ pixmap_art.cpp
@@ -5,10 +5,10 @@ using namespace agl;
 int main(int argc, char** argv) {
     ppm_image image;
     image.load("../images/cat-ascii.ppm");
-    image.rotate().save("rotated.ppm");
-    image.invert().save("inverted.ppm");
+    image.rotate().save("rotate.ppm");
+    image.invert().save("invert.ppm");
     image.border({255, 0, 0}, 25).save("border.ppm");
-    image.box_blur(25).save("box_blurred.ppm");
-    image.swirl_color().save("swirled.ppm");
+    image.box_blur(25).save("box_blur.ppm");
+    image.swirl_color().save("swirl.ppm");
     image.overlay({191, 139, 225}).save("overlay.ppm");
 }
